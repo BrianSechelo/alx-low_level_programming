6ddba4c5568c129f5a2e946e9801b2808dae32db
BrianSechelo <sechbrian123@gmail.com>
2024-05-07 11:38:21 -0700

    Add task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,36 +8,37 @@
 /**
  * struct listint_s - singly linked list
  *
- * @n:interger
- * @index:Index of the node in the list
+ * @n: Integer
+ * @index: Index of the node in the list
  * @next: Pointer to the next node
- * Description:singly linked list node structure
- * for Holbertion project
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
-}listint_t;
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
 
 /**
- * struct skiplist_s- Singly linked list with an express lane
+ * struct skiplist_s - Singly linked list with an express lane
  *
- * @n:interger
- * @index:Index of the node in the list
- * @next:Pointer to the next node
- * @express:Pointer to the next node in the lane
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
  *
  */
-
 typedef struct skiplist_s
 {
+
 	int n;
 	size_t index;
 	struct skiplist_s *next;
 	struct skiplist_s *express;
-}skiplist_t;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
